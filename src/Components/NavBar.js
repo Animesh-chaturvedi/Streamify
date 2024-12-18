3555e48536413f7b23d551dfab1739e071bf04ab
Animesh Chaturvedi <animesh.chaturvedi.1234@gmail.com>
2024-12-18 09:32:15 +0530

    bug fix for building

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-center position-fixed start-0 top-0 end-0">
-      <a className="navbar-brand" href="#" style={{ color: 'rgb(72, 149, 247)', fontSize: '24px', fontWeight: 700 }}>
+      <a className="navbar-brand" href='/' style={{ color: 'rgb(72, 149, 247)', fontSize: '24px', fontWeight: 700 }}>
         Streamify
       </a>
     </nav>
